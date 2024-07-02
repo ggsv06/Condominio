30e0b25b8b3560efa7807384b0b69173ddcdc19d
ggsv06 <g278439@dac.unicamp.br>
2024-07-01 22:04:34 -0300

    alfa 1.0

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-# Ler tabela de dados do registro m^3
-# Gerar nova tabela com a diferença entre os valores dos registros nos últimos dois meses
-# Multiplicar a diferença com o fator respectivo
-# Gerar tabela final com o preço e consumo
-
